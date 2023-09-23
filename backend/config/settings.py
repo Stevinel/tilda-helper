2fa79d01e4fe8f4e15bdca7b118af6b74500ad41
Stanislav Voronov <stevinel@xaker.ru>
2023-09-24 00:33:35 +0300

    test(project): тестирование функционала

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,7 +1,10 @@
+import logging
 import os
 from pathlib import Path
 
 from celery import Celery
+
+logging.getLogger("child").propagate = False
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
