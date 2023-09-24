2be39922ea73045c73415780f326f8134e2194e8
Stanislav Voronov <stevinel@xaker.ru>
2023-09-24 04:20:56 +0300

    test(project): тестирование функционала

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
-python apps/telegram_bot/main.py
 gunicorn config.wsgi:application -b 0.0.0.0:8000 --reload & celery -A config worker
+python apps/telegram_bot/main.py
