b4609b1162917852f9b64c44b4e9416c6a1e69b0
Stanislav Voronov <stevinel@xaker.ru>
2024-02-04 15:20:28 +0300

    feat(cicd): фикс админки джанго 4+

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -21,7 +21,11 @@ DEBUG = os.environ.get('DEBUG', False) == 'True'
 ALLOWED_HOSTS = [x.strip() for x in os.getenv('ALLOWED_HOSTS').split(',')]
 CORS_ORIGIN_WHITELIST = [x.strip() for x in os.getenv('ALLOWED_HOSTS').split(',')]
 DNS = os.getenv('DNS')
-CSRF_TRUSTED_ORIGINS = [os.getenv('CSRF_TRUSTED_ORIGINS', (f'https://{DNS}', f'http://{DNS}'))]
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS')
+if CSRF_TRUSTED_ORIGINS:
+    CSRF_TRUSTED_ORIGINS = CSRF_TRUSTED_ORIGINS.split(';')
+else:
+    CSRF_TRUSTED_ORIGINS = [f'https://{DNS}', f'http://{DNS}']
 
 # Application definition
 INSTALLED_APPS = [
